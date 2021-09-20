2115f86de5a608bc5be25c5ac490d48656b92405
Katya Menyaylo <katya/menyaylo/04@gmail.com>
2021-09-20 21:35:50 +0300

    пробуем снова!

--- 090921.py
+++ 090921.py
@@ -1,14 +1,24 @@
-def gen_names(first_names):
-    for i in range(first_names):
-        name = ''
-        name = chr(random.randint(65, 90))
-        rnd_num = random.randint(3, 10)
-        for u in range(rnd_num):
-            name += chr(random.randint(97, 122))
-        names.append(name)
-    return names
+from random import choice
 
-print('Введите количество имен:')
-n = int(input())
-b = decorator(gen_names)
-print(b(n)) 
+big_letters = [i for i in range(65, 91)]
+little_letters = [i for i in range(97, 123)]
+
+
+def gen_name(big_letters, little_letters, num):
+    i = 1
+    while i < num:
+        s = ''
+        for x in range(0, 5):
+            s += chr(choice(big_letters))
+            for y in range(0, 9):
+                s += chr(choice(little_letters))
+
+            if x < 4:
+                s += ' '
+        yield s
+        i += 1
+
+
+ranger = gen_name(big_letters, little_letters, 5)
+result = [x for x in ranger]
+print(result)
