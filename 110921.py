fce0d476f2d9b16dd1bfe0926aea04b1dbb7082a
Katya Menyaylo <katya/menyaylo/04@gmail.com>
2021-10-09 12:32:22 +0300

    дз за 11 число

--- 110921.py
+++ 110921.py
@@ -1,51 +1,67 @@
 import string
 import random
-
 list_=[]
-
 def rnd_string (n):
     a = ''
     for i in range(n):
         a += random.choice(string.ascii_letters)
     return a
-
 def char_count(n):
     length_ = len(n)
     count_up = 0
     count_down = 0
     for i in range(length_):
         if n[i] in string.ascii_lowercase:
+        if n[i] in string.ascii_uppercase:
             count_up += 1
         else:
             count_down += 1
     if count_up == count_down:
         return -1
+        return 0
     elif count_up > count_down:
         return 1
     else:
         return 0
+        return -1
 
-def masslal(n,k):
-    massslala = []
+def mass(n,k):
+    masss = []
     for i in range(k):
         b = rnd_string(n)
-        massslala.append(b)
-    return massslala
+        masss.append(b)
+    return masss
 
 def procent(mas, n):
-    count_masslal = len(masslal(n,k))
+def procent(n, q):
+    count_mass = len(mass(n,k))
+    ku = 0
+    ka = 0
     ko = 0
-    for i in range(count_masslal):
-        if char_count(masslal(n, k)[i]) == 1:
+    for i in range(count_mass):
+        if char_count(mass(n, k)[i]) == -1:
+            ka += 1
+        if char_count(mass(n, k)[i]) == 1:
             ko += 1
-    return (ko/count_masslal*100)
+    return (ko/count_mass*100)
+        if char_count(mass(n, k)[i]) == 0:
+            ku += 1
+    if q == 3:
+        return (ko/count_mass*100)
+    if q == 1:
+        return (ka/count_mass*100)
+    if q == 2:
+        return (ku/count_mass*100)
 
 
-print('Введите кол-во символов в строке')
+print('Введите количество символов в строке')
 n = int(input())
-print('Введите кол-во строк')
+print('Введите количество строк')
 k = int(input())
 
-massiv_strings = (masslal(n,k))
+massiv_strings = (mass(n,k))
 print(massiv_strings)
 print('Количество строк с бОльшим количеством букв в верхнем регистре = ', procent(massiv_strings, n), '%', sep='') 
+print('Количество строк с бОльшим количеством букв в верхнем регистре = ', procent(n, 3), '%', sep='')
+print('Количество строк с бОльшим количеством букв в нижнем регистре = ', procent(n, 1), '%', sep='')
+print('Количество строк с одинаковым количество букв в верхнем и нижнем регистре = ', procent(n, 2), '%', sep='')
